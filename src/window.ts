54639f4f3339550b9b0f11b2284263531fd20974
Nardus Serfontein <nserfontein@ekomi-group.com>
2024-02-22 11:51:09 +0200

    Toggle button updated to switch off when clicking outside the page fixed removed previous commits

--- src/window.ts
+++ src/window.ts
@@ -1,10 +1,5 @@
 import { observersConfigType } from './components/Bot';
-import { ButtonTheme } from './features/bubble/types';
 
-type Props = ButtonTheme & {
-  isBotOpened: boolean;
-  toggleBot: () => void;
-};
 /* eslint-disable solid/reactivity */
 type BotProps = {
   chatflowid: string;
@@ -18,35 +13,11 @@ export const initFull = (props: BotProps & { id?: string }) => {
   if (!fullElement) throw new Error('<flowise-fullchatbot> element not found.');
   Object.assign(fullElement, props);
 };
-const isClickInsideChatbot = (target: any, chatbotElement: any) => {
-  return chatbotElement.contains(target);
-};
-export const init = (props: BotProps, propsBtn: Props) => {
+
+export const init = (props: BotProps) => {
   const element = document.createElement('flowise-chatbot');
   Object.assign(element, props);
-
-  // Function to add the event listener to the body
-  const addBodyClickListener = () => {
-    // Define a new event handler
-    const clickOutsideHandler = (event:any) => {
-      const chatbotElement = document.querySelector('flowise-chatbot');
-      if (chatbotElement && !isClickInsideChatbot(event.target, chatbotElement)) {
-        propsBtn.toggleBot();
-      }
-    };
-
-    // Add the event listener to the body
-    document.body.addEventListener('click', clickOutsideHandler);
-  };
-
-  // Append the chatbot element to the body
   document.body.appendChild(element);
-
-  if (document.readyState === 'loading') {  // The document is still loading
-    document.addEventListener('DOMContentLoaded', addBodyClickListener);
-  } else {  // The document is already loaded
-    addBodyClickListener();
-  }
 };
 
 type Chatbot = {
