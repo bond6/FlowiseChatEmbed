694ae7829a44fd6c9b3c3f73865e2639e48a4afb
Nardus Serfontein <nserfontein@ekomi-group.com>
2024-02-22 11:48:51 +0200

    Toggle button updated to switch off when clicking outside the page V2

--- src/window.ts
+++ src/window.ts
@@ -18,27 +18,35 @@ export const initFull = (props: BotProps & { id?: string }) => {
   if (!fullElement) throw new Error('<flowise-fullchatbot> element not found.');
   Object.assign(fullElement, props);
 };
-const isClickInsideChatbot = (target:any, chatbotElement:any) => {
+const isClickInsideChatbot = (target: any, chatbotElement: any) => {
   return chatbotElement.contains(target);
 };
 export const init = (props: BotProps, propsBtn: Props) => {
   const element = document.createElement('flowise-chatbot');
-  
-  
-
   Object.assign(element, props);
-  document.body.appendChild(element);
-    // Add event listener to the body
-    document.body.addEventListener('click', (event) => {
-      // Assuming `chatbotElement` is the DOM element of your chatbot
+
+  // Function to add the event listener to the body
+  const addBodyClickListener = () => {
+    // Define a new event handler
+    const clickOutsideHandler = (event:any) => {
       const chatbotElement = document.querySelector('flowise-chatbot');
-    
-      // If the click is not inside the chatbot, toggle it
-      if (!isClickInsideChatbot(event.target, chatbotElement)) {
-        // Assuming `toggleBot` is the function that toggles the chatbot visibility
+      if (chatbotElement && !isClickInsideChatbot(event.target, chatbotElement)) {
         propsBtn.toggleBot();
       }
-    });
+    };
+
+    // Add the event listener to the body
+    document.body.addEventListener('click', clickOutsideHandler);
+  };
+
+  // Append the chatbot element to the body
+  document.body.appendChild(element);
+
+  if (document.readyState === 'loading') {  // The document is still loading
+    document.addEventListener('DOMContentLoaded', addBodyClickListener);
+  } else {  // The document is already loaded
+    addBodyClickListener();
+  }
 };
 
 type Chatbot = {
